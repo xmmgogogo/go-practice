f65bf56e8bcb900909b4b0b4db61138c63581ae1
m <m@gmail.com>
2022-12-28 00:41:48 +0800

    chann通道练习

---
+++ count_str.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	inputL = make(chan string, 2)
+	//outPutL = make(chan string, 2)
+)
+
+func main() {
+	// 计算一组字符里面，某个字母的数量
+	list := []string{"app", "apq", "恭喜a", "恭喜b"}
+
+	//fmt.Println(count1(list))
+	//sliceChar()
+	fmt.Println(count2(list))
+}
+
+func count2(items []string) (res map[string]int) {
+	res = make(map[string]int)
+
+	//maxCount := len(items)
+	go func() {
+		for _, v := range items {
+			inputL <- v
+		}
+		close(inputL)
+	}()
+
+	// 异步等待的程序
+	for {
+		// 这一行很重要，如果通道关闭了，则直接退出for循环即可
+		if getOne, isOk := <-inputL; isOk {
+			fmt.Println("读取到一个值", getOne)
+			for _, v := range getOne {
+				res[string(v)]++
+			}
+		} else {
+			break
+		}
+	}
+
+	return
+}
+
+func count1(items []string) map[string]int {
+	var countMap = make(map[string]int)
+
+	for _, v := range items {
+		for _, v1 := range v {
+			countMap[string(v1)]++
+		}
+	}
+
+	return countMap
+}
+
+func sliceChar() {
+	word := "abc"
+	fmt.Println(word[:1]) // a
+
+	word2 := "中国abc"
+	fmt.Println(word2[:1]) // a
+	fmt.Println(word2[:3]) // a
+	fmt.Println(word2[:5]) // a
+	fmt.Println(word2[:6]) // a
+
+	word3 := []rune(word2)
+	fmt.Println(string(word3[:1])) // a
+	fmt.Println(string(word3[:2])) // a
+	fmt.Println(string(word3[:3])) // a
+	fmt.Println(string(word3[:4])) // a
+	fmt.Println(string(word3[:5])) // a
+
+}
---
+++ producer_consumer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// 统计总和
+
+	var (
+		ch    = make(chan string)
+		items = []string{"ap", "ad", "dd", "d2"}
+	)
+	go producer(items, ch)
+	consumer(ch)
+}
+
+func producer(items []string, ch chan string) {
+	for _, v := range items {
+		ch <- v
+	}
+	close(ch)
+}
+
+func consumer(ch chan string) {
+	// 为什么读完不会阻塞？？？
+	//for _, v := range <-ch {
+	//	fmt.Println("获取的内容是:", string(v))
+	//}
+
+	for {
+		if word, isOk := <-ch; isOk {
+			fmt.Println("获取的内容是:", word)
+			time.Sleep(time.Millisecond * 100)
+		} else {
+			fmt.Println("读取结束")
+			break
+		}
+	}
+}
